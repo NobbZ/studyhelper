5dc91ae0371948e6a5b4195dcde3c3bd85419ffb
Norbert Melzer <timmelzer@gmail.com>
2013-12-12 22:50:41 +0100

    Version bumped to 0.0.1

--- Study Helper.tamper.js
+++ Study Helper.tamper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Study Helper
 // @namespace   http://userscript.nobbz.de/
-// @version     0.0.0
+// @version     0.0.1
 // @downloadURL https://github.com/NobbZ/studyhelper/raw/master/Study%20Helper.tamper.js
 // @description A small handfull of helpers for the game teacher-story
 // @match       http://teacher-story.com/
@@ -10,7 +10,13 @@
 // @require     http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
+var shVersion = GM_info.script.version;
+
+console.log("Study Helper v" + shVersion + " started");
+
 var xpVal	= $(".bar").data("val");
 var xpMax	= $(".bar").data("max");
 
 $(".progressBar.teacherXP").text(xpVal + "/" + xpMax + " (" + (xpMax - xpVal) + ")");
+
+console.log("Study Helper v" + shVersion + " finished");
